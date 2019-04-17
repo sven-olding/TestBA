6fcfe5175f28205fc7458c077dbd32a39f8915ba
Sven Olding <sven.olding@gmx.de>
2019-04-17 16:58:02 +0200

    add action class (c#) for new action

---
+++ TestBA/Configuration/Navigation/ClientAction/MarkCompleteAction.cs
@@ -0,0 +1,27 @@
+﻿using BA.Core.Configuration.Navigation.Client;
+using BA.Core.CustomAttributes;
+using BA.Core.Enums;
+using System;
+using System.Linq.Expressions;
+
+namespace TestBA.Configuration.Navigation.ClientAction
+{
+    [Serializable]
+    [Toolbox(EnumConfigurationType.AttributeValues.NavigationConfiguration)]
+    [ControlFilter("NavigationConfigurationType", ExpressionType.Equal, EnumNavigationConfigurationType.AttributeValues.RibbonNavigation, EnumControlFilterApplyState.IfPositive)]
+    public class MarkCompleteAction : ClientActionBase
+    {
+        public MarkCompleteAction() : base()
+        {
+            ToolboxName = "Complete Task";
+            Caption = "Complete Task";
+            ControlInitName = "CompleteText";
+            ToolboxGroupName = "Custom Actions";
+            Id = new Guid("F9FE2AC7-981B-4044-A2B7-7CF271826610");
+            Icon = "projector";
+            IconName = Icon;
+            DesignerHintText = "Marks selected tasks as complete";
+            VisibilityForParentTypes.Add(EnumActionVisibleForParentType.Values.Grid);
+        }
+    }
+}
